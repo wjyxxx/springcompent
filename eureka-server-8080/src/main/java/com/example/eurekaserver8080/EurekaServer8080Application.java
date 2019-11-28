5fcf061b99e636413fb81355dda05aad659b0fc6
wjyxxx <w18829897653@163.com>
2019-11-28 13:44:55 +0800

    second commit code

--- eureka-server-8080/src/main/java/com/example/eurekaserver8080/EurekaServer8080Application.java
+++ eureka-server-8080/src/main/java/com/example/eurekaserver8080/EurekaServer8080Application.java
@@ -2,11 +2,13 @@ package com.example.eurekaserver8080;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * eureka服务端
  */
+@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer8080Application {
